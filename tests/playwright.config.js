0bf9a038289bd15e031065ca43b5a3d01f05ee55
Pravingautam <142480645+Pravingautam@users.noreply.github.com>
2026-06-26 15:02:16 +0530

    Change test directory to root in Playwright config

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: './',
   use: {
     headless: true,
     baseURL: 'http://localhost:3000',
