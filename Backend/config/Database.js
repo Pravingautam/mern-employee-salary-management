4086091a466fcf602f3130982ef3735082949c74
Pravingautam <142480645+Pravingautam@users.noreply.github.com>
2026-06-26 20:34:25 +0530

    Update Database.js to use environment variables
    Refactored database connection to use environment variables for configuration.

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -1,8 +1,13 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
-const db = new Sequelize('db_penggajian3', 'root', '', {
-    host: "localhost",
-    dialect: "mysql"
-});
+const db = new Sequelize(
+  process.env.DB_NAME || "db_penggajian3",
+  process.env.DB_USER || "root",
+  process.env.DB_PASSWORD || "root",
+  {
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+  }
+);
 
 export default db;
